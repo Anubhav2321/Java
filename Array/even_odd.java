5875190dab7b17cbdac1f88772211ad8df934f1c
Anubhav2321 <anubhavsamanta2005@gmail.com>
2026-03-27 18:06:58 +0530

    Array

---
+++ Array/even_odd.java
@@ -0,0 +1,17 @@
+package Array;
+
+public class even_odd {
+    public static void main(String[] args) {
+        int[] arr = {1,2,3,4,5,6,7,8,9,10};
+        int even =0, odd =0;
+        for(int x: arr){
+            if(x%2 == 0){
+                even++;
+            }else{
+                odd++;
+            }
+        }
+        System.out.println("Even numbers: " + even);
+        System.out.println("Odd numbers: " + odd);
+    }
+}
