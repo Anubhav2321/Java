d3bd7832739520bcb5a9111597d9d50efd76fb47
Anubhav2321 <anubhavsamanta2005@gmail.com>
2026-03-27 19:29:32 +0530

    String

---
+++ Strings/Important_String_Methods.java
@@ -0,0 +1,21 @@
+package Strings;
+
+public class Important_String_Methods {
+    public static void main(String[] args) {
+        String str1 = "Hello";
+        String str2 = "my name is anubhav ";
+        String str3 = "I AM AK BCA FINAL YEAR STUDENT";
+
+        // length() method
+        System.out.println("Length of str1: " + str1.length());
+        // toUpperCase() method
+        System.out.println("str2 in uppercase: " + str2.toUpperCase());
+        // toLowerCase() method
+        System.out.println("str3 in lowercase: " + str3.toLowerCase());
+        // charAt() method
+        System.out.println("Character at index 2 in str1: " + str1.charAt(2));
+        
+
+
+    }
+}
