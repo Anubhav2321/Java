bd2838bb773a61b33333d6720b213a1af5150b2b
Anubhav2321 <anubhavsamanta2005@gmail.com>
2026-03-29 10:08:02 +0530

    reverse string

---
+++ Strings/reverse.java
@@ -0,0 +1,15 @@
+package Strings;
+
+public class reverse {
+    public static void main(String[] args) {
+        String str = "Anubhav Samanta";
+        String rev = "";
+
+        for (int i = str.length() - 1; i >=0; i--) {
+            rev += str.charAt(i);
+        }
+        System.out.println("Reversed string: " + rev);
+
+    }
+
+}
