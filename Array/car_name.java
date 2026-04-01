29bf5c47b9f7fd3f53625ea0b9f32f70aeeb891e
Anubhav Samanta <anubhavsamanta2005@gmail.com>
2026-04-01 09:21:31 +0530

    Array

---
+++ Array/car_name.java
@@ -0,0 +1,12 @@
+package Array;
+
+public class car_name {
+    public static void main(String[] args) {
+        String[] cars = {"Volvo", "BMW" , "Audi", "Mustang", "Ferrari", "Lamborghini", "Porsche", "Mercedes-Benz", "Toyota", "Honda"};
+        for (int i=0; i<cars.length; i++){
+            System.out.println(cars[i]);
+            
+        }
+    }
+    
+}
---
+++ Array/input.java
@@ -0,0 +1,14 @@
+package Array;
+import java.util.Scanner;
+public class input {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner (System.in);
+        System.out.print("Enter your fabourite car name:-");
+        String car = sc.nextLine();
+        System.out.println("Your favourite car is: " + car);
+        sc.close();
+    }
+   
+    
+    
+}
