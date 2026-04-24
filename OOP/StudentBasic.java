e1820a755e705c1a6ac92cb75664303b9c397e30
Anubhav Samanta <anubhavsamanta2005@gmail.com>
2026-04-24 22:21:59 +0530

    OOPS

---
+++ OOP/StudentBasic.java
@@ -0,0 +1,16 @@
+package OOP;
+
+public class StudentBasic {
+    String name;
+    int age ;
+
+    public static void main(String[] args) {
+         StudentBasic s1 = new StudentBasic();
+         s1.name = "Anubhav";
+         s1.age = 21;
+         System.out.println("Name: " + s1.name + ", Age: " + s1.age);
+
+    }
+
+
+}
